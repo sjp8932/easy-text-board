aeb89225d380a981fdcd829609a11b43588c84b9
sjp8932 <sjp8932@gmail.com>
2020-10-30 15:28:40 +0900

    스캐너로 고객에게 명령어 입력받기

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -1,7 +1,32 @@
 package com.sbs.example.easytextboard;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("안녕");
-	}
+		int x; // 미지수 x를 생성
+		x=5;
+		System.out.println(x);
+		x=2;
+		System.out.println(x);
+	
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("명령어를 입력해주세요 ");
+		String command = scanner.nextLine();
+		
+		
+		
+		if(command.equals("article add")) {
+			System.out.println("== 게시물 등록 ==");
+		}
+		else if(command.equals("article list")) {
+			System.out.println("== 게시물 리스트 ==");
+		}else {
+			System.out.println("== 존재하지 않는 명령어 ==");
+		}
+		
+		scanner.close();
+	
+	}	
 }

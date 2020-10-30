96e546e6e3e6e0d00e8525002ddc3aace0e2696a
sjp8932 <sjp8932@gmail.com>
2020-10-30 15:48:16 +0900

    while, 무한반복, 고객이 종료를 원할 때 까지 실행되도록 수정

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		int x; // 미지수 x를 생성
-		x=5;
+		x = 5;
 		System.out.println(x);
-		x=2;
+		x = 2;
 		System.out.println(x);
-	
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("명령어를 입력해주세요 ");
 		String command = scanner.nextLine();
-		
-		
-		
-		if(command.equals("article add")) {
-			System.out.println("== 게시물 등록 ==");
+
+		while (true) {
+			if (command.equals("article add")) {
+				System.out.println("== 게시물 등록 ==");
+			} else if (command.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+			} else if (command.equals("system exit")) {
+				System.out.println("== 프로그램 종료 ==");
+				break;
+			}
+			else {
+				System.out.println("== 존재하지 않는 명령어 ==");
+				
+			}
 		}
-		else if(command.equals("article list")) {
-			System.out.println("== 게시물 리스트 ==");
-		}else {
-			System.out.println("== 존재하지 않는 명령어 ==");
-		}
-		
 		scanner.close();
-	
-	}	
+
+	}
 }

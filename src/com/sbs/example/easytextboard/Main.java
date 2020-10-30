6119987f5a492cb690e18617a140949f79b48ef6
sjp8932 <sjp8932@gmail.com>
2020-10-30 17:42:17 +0900

    게시물 작성시 번호가 자동으로 증가하도록 처리

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
+		int lastarticle = 0;
+		
 		while (true) {
 			System.out.printf("명령어) ");
 			String command = scanner.nextLine();
@@ -13,7 +15,8 @@ public class Main {
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 				
-				int id = 1;
+				
+				int id = lastarticle + 1;
 				String title;
 				String body;
 				
@@ -22,12 +25,13 @@ public class Main {
 				title = scanner.nextLine();
 				System.out.printf("내용 : ");
 				body = scanner.nextLine();
-		
+				
+				lastarticle = id;
 				
 				System.out.println("== 생성된 게시물 정보 == ");
-				System.out.println("번호 : " + id);
-				System.out.println("타이틀 : " + title);
-				System.out.println("내용 : " + body);
+				System.out.printf("번호 : %d\n", id);
+				System.out.printf("제목 : %s\n", title);
+				System.out.printf("내용 : %s\n", body);
 				
 				
 			} else if (command.equals("article list")) {

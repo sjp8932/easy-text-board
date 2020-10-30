facf9b5f89cbe7253e896ce0be50c9b567bcf0ea
sjp8932 <sjp8932@gmail.com>
2020-10-30 16:29:16 +0900

    게시물 등록 시 정보입력받는 기능 구현

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -4,20 +4,32 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		int x; // 미지수 x를 생성
-		x = 5;
-		System.out.println(x);
-		x = 2;
-		System.out.println(x);
-
 		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("명령어를 입력해주세요 ");
-		String command = scanner.nextLine();
-
+		
 		while (true) {
+			System.out.printf("명령어) ");
+			String command = scanner.nextLine();
+			
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
+				
+				int id = 1;
+				String title;
+				String body;
+				
+				
+				System.out.printf("제목 : " );
+				title = scanner.nextLine();
+				System.out.printf("내용 : ");
+				body = scanner.nextLine();
+		
+				
+				System.out.println("== 생성된 게시물 정보 == ");
+				System.out.println("번호 : " + id);
+				System.out.println("타이틀 : " + title);
+				System.out.println("내용 : " + body);
+				
+				
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 			} else if (command.equals("system exit")) {
@@ -26,7 +38,7 @@ public class Main {
 			}
 			else {
 				System.out.println("== 존재하지 않는 명령어 ==");
-				
+			
 			}
 		}
 		scanner.close();
